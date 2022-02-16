7c4091e9ff08ce441f871d5a93d6e5c80b878149
fpdsjr <flaviopds.jr@gmail.com>
2022-02-16 15:31:32 -0300

    update: update routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,16 @@
 import { Router } from "express"
 import { CreateToDoController } from "./modules/todo/useCases/createTodo/CreateToDoController";
-
-
+import { ListAllToDoController } from "./modules/todo/useCases/listAllToDo/ListAllToDoController";
 
 const routes = Router();
 
 const createToDo = new CreateToDoController();
-
-
+const listAllTodo = new ListAllToDoController()
 
 
 
 routes.post("/todo", createToDo.handle)
 
+routes.get("/listalltodo",  listAllTodo.handle)
 
 export { routes }
